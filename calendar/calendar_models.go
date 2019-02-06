42f2ebccfd9391aaebd0be1015f9b66e86d98afa
Jan-Cedric Anslinger <jc.anslinger@mintme.io>
2019-02-06 17:43:06 +0100

    Updated models

--- calendar/calendar_models.go
+++ calendar/calendar_models.go
@@ -1,8 +1,9 @@
 package calendar
 
 type Day struct {
-	DateDay 	int	 // The number of this day >> lke 22.
-	IsActive 	bool // Whether the day shall be highlighted because its today's day
+	DateDay 	int	   // The number of this day >> lke 22.
+	Month 		string // The month of this day
+	IsActive 	bool   // Whether the day shall be highlighted because its today's day
 }
 
 type CalendarWeekPage struct {
@@ -11,4 +12,5 @@ type CalendarWeekPage struct {
 	Month 		string 	// The month the selected week is in
 	KWWeek		string	// The week of the year
 	Days     	[]Day	// All days of this week
+	// TODO: Add prev link and next link
 }
